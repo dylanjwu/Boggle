519ddc440cb4e68ae25104e0fee86aaed4b298b9
[dylanjwu] <dylanjwu@gmail.com>
2020-06-14 16:54:49 -0400

    working timer, but doesn't restart with shuffle...hmmm, strange behavior

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,70 +1,52 @@
-//Code will not work, do NOT run
+let endSecs;
+let endMins;
+let totalSecs;
+let totalMins;
+let intervalId;
 
-// let d = new Date();
-// let startMin = d.getMinutes();
-// let startSec = d.getSeconds();
-let start = new Date();
-let endMin = start.getMinutes() + 1;
-//make it 3 mins
-function myTimer() {
-    let d = new Date();
-    let secs, mins;
+function getTime() {
+    let time = new Date();
+    let mins = time.getMinutes();
+    let secs = time.getSeconds();
 
-    console.log(secs);
-    console.log(mins);
 
-    document.getElementById("timer").innerHTML = d.toLocaleTimeString();
-    if (d.getMinutes() === endMin) {
-        confirmation = confirm("22 mins");
-        endMin += 3;
+    if (totalMins === 0 && totalSecs === 0) {
+        alert("done");
+        done = true;
+        clearInterval(intervalId);
         return;
     }
-    // if(secs === (startS % 60) && mins === Number(startM / 60)) {
-    //     // document.getElementById("timer").innerHTML = secs;
-    //     alert();
-    // }
+
+    if (secs != lastSec) {
+        if (totalSecs > 0)
+            totalSecs -= 1;
+        else {
+            totalSecs = 59;
+        }
+
+        lastSec = secs;
+    }
+
+    if (totalSecs === 59 && totalMins > 0) {
+        totalMins -= 1;
+    }
+
+    console.log(totalMins);
+    console.log(totalSecs);
+    if (totalSecs < 10) {
+        document.getElementById('timer').textContent = totalMins + ':0' + totalSecs;
+    } else {
+        document.getElementById('timer').textContent = totalMins + ':' + totalSecs;
+    }
 }
 
-function startTimer() {
-    // document.getElementById("timer").innerHTML = '00:00';
-
-    setInterval(myTimer, 1000);
-
-}
-
-startTimer();
-
-
-{
-    /* <script>
-        // Set the date we're counting down to
-        var current = new Date().getTime();
-        var countDownDate = new Date().getTime();
-
-        // Update the count down every 1 second
-        var x = setInterval(function() {
-
-            // Get today's date and time
-            var now = new Date().getTime();
-
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            // Output the result in an element with id="demo"
-            document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-                minutes + "m " + seconds + "s ";
-
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "EXPIRED";
-            }
-        }, 1000);
-    </script> */
+function startTimer(mins) {
+    endTime = new Date();
+    endSecs = endTime.getSeconds();
+    endMins = endTime.getMinutes();
+    lastSec = endSecs;
+    totalMins = mins;
+    intervalId = setInterval(getTime, 1000);
+    console.log('here');
+    return;
 }

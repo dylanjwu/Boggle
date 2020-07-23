e96e46fedefaa7e5f58c0e15d8330ceb3b38d5c9
[dylanjwu] <dylanjwu@gmail.com>
2020-07-23 09:18:00 -0400

    Rewrite req as async/.then hybrid

--- scripts/ajaxRequest.js
+++ scripts/ajaxRequest.js
@@ -1,12 +1,42 @@
 let allWords = [];
 
-const promise = fetch('../assets/words.txt').then(response =>
-    response.text()
-).then(function(words) {
-    allWords = words.split('\n');
-}).catch(err => console.log(err.message));
 
-promise.then(() => console.log(allWords));
+async function myFetch() {
+    let promise = await fetch('../assets/words.txt')
+    return await promise.text();
+}
+
+async function splitText(words) {
+    allWords = await words.split('\n');
+    return await words.split('\n');
+}
+
+myFetch().then(text =>
+        splitText(text))
+    .then(splitted => console.log(splitted))
+    .catch(err => console.log(err.message));
+console.log(allWords);
+
+// promise.then(function(words) {
+//     allWords = words.split('\n');
+// }).catch(err => console.log(err.message));
+
+// promise.then(() => console.log(allWords));
+
+
+// const promise = fetch('../assets/words.txt').then(response =>
+//     response.text()
+// ).then(function(words) {
+//     allWords = words.split('\n');
+// }).catch(err => console.log(err.message));
+
+// promise.then(() => console.log(allWords));
+
+
+
+
+
+
 
 
 // function makeRequest(callback) {
